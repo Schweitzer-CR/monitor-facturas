e78bce811a25ca88fbc7f0b6e0c369b96f0127f0
Jordan Mora <jordanmcastro3@gmail.com>
2026-05-27 21:27:49 -0600

    Add files via upload

--- config/urls.py
+++ config/urls.py
@@ -6,13 +6,15 @@ from facturas.views import monitor_view, login_view, factura_detalle, logout_vie
 from django.urls import path
 from facturas import views
 
+
+app_name = 'facturas'
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', monitor_view, name='monitor'),
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
     path('factura/<int:pk>/', factura_detalle, name='detalle'), # Esta es la ruta del detalle
-    path('exportar/excel/', views.exportar_facturas_excel, name='exportar_excel'),
+    path('exportar-excel/', views.exportar_facturas_excel, name='exportar_facturas_excel'),
     path('configurar/', views.guardar_configuracion, name='guardar_config'),
     path('guardar-cliente/', views.guardar_cliente, name='guardar_cliente'),
     path('guardar-config/', views.guardar_configuracion, name='guardar_config'),
